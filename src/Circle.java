7856b67031b93bbe0416b62aaa28cc04172c80b2
takut <takut@10.80.156.128>
2025-11-28 15:33:59 +0900

    1

--- src/Circle.java
+++ src/Circle.java
@@ -1,37 +1,39 @@
 
 public class Circle extends MyFrame2 {
-
-	/*public void drawCircle(int cx, int cy, int r) {
-	    // TODO: メソッドの中身を書く
-		double x;
-		double y;
-		for(int i=0; i<360; i++) {
-			x = r*Math.cos(Math.toRadians(i))+cx;
-			y = r*Math.sin(Math.toRadians(i))+cy;
-			
-			fillRect((int)x,(int)y,1,1);
-		}
-	}*/
-    
+	
     public void drawSpiral(int cx, int cy, int dist, int num, int deg) {
     	double x;
 		double y;
-		for(int i=0; i<360; i++) {
-			x = r*Math.cos(Math.toRadians(i))+cx;
-			y = r*Math.sin(Math.toRadians(i))+cy;
+		int total=360*num;
+		
+		for(int i=0; i<total; i++) {
+			double rate=(double)i/total;
+			double r=dist*rate;
+			
+			x = r*Math.cos(Math.toRadians(deg+i))+cx;
+			y = r*Math.sin(Math.toRadians(deg+i))+cy;
 			
 			fillRect((int)x,(int)y,1,1);
 		}
     }
+    
     public void run() {
-        // TODO: drawCircle を呼出して，半径が異なる n 個の同心円を表示
-    	for(int i=0; i<7+3; i++) {
-    		//drawCircle(150,150,(i+1)*10);
-    		
+    	setSize(600,600);
+    	
+    	for(int i=0; i<6; i++) {
+    		if(i<3) {
+    			drawSpiral(100+i*200,200,70,i+1,0);
+    		}else {
+    			drawSpiral(100+(i-3)*200,400,70,4,90+90*(i-3));
+    		}
     	}
     }
 
     public static void main(String[] args) {
         new Circle();
+        
     }
 }
+
+
+
